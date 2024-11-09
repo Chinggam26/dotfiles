c9d10df1bcad0a1cefbf81901976768a74487f7c
Adarsh Maurya <chotuport@gmail.com>
2024-11-09 16:22:30 +0530

    Added pyrigtt and jdtls

--- all/.config/nvim/lua/core/plugin_config/mason.lua
+++ all/.config/nvim/lua/core/plugin_config/mason.lua
@@ -9,3 +9,12 @@ require("mason").setup({
     }
 })
 require("mason-lspconfig").setup()
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+require("lspconfig")["pyright"].setup {
+  capabilities = capabilities
+}
+require("lspconfig")["jdtls"].setup {
+  capabilities = capabilities
+}
